85cf8281e61cb494b26681112c531648c942c7e1
Edmon Sebit <edmontheprogrammer@gmail.com>
2024-06-15 10:22:32 -0600

    Creating the useFetch component

--- nba-players/src/useFetch.tsx
+++ nba-players/src/useFetch.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
 
-const useFetch = () => {
-  return (
-    <div>useFetch</div>
-  )
+
+interface UseFetchResult {
+
+    data: any | null; 
+    isPending: boolean; 
+    error: any | null; 
 }
 
-export default useFetch
+
+
+const useFetch = (url: string): UseFetchResult => {
+
+
+    const [data, setData] =  useState<any | null>(null);
+    const [isPending, setIsPending] = useState<boolean>(true);
+    const [error, setError] = useState(any | null)(null); 
+
+
+
+    useEffect( () => {
+       
+        setTimeout(() => {
+            fetch(url)
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error('Error fetching users data');
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setData(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
+                });
+        }, 1000);
+    
+    
+    }, [url]);
+
+  return {data, isPending, error};
+};
+
+export default useFetch;
