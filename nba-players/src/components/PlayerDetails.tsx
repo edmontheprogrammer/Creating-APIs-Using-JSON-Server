2091967d7d68e6f7b7cc96556c4c13e70ce6c136
Edmon Sebit <edmontheprogrammer@gmail.com>
2024-06-15 11:06:44 -0600

    adding the PlayerDetails component

--- nba-players/src/components/PlayerDetails.tsx
+++ nba-players/src/components/PlayerDetails.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import useFetch from '../useFetch'
 
 const PlayerDetails = () => {
+
+    const {id} = useParams();
+    const {data: player, error, isPending} = useFetch("http://localhost:8000/players/" + id);
+
+
   return (
-    <div>PlayerDetails</div>
+    <>
+            <section>
+                {isPending && <p>Loading player details...</p>}
+
+                {error && <p>{error}</p>}
+
+                {player && (
+                    <>
+                        <h1>Player {player.id} details</h1>
+                        <h2>{player.name}</h2>
+                        <p>{player.email}</p>
+                        <p>{player.phone}</p>
+                        <p>{player.bio}</p>
+                    </>
+                )}
+            </section>
+    </>
   )
 }
 
